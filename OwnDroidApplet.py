8cca33e3930c1dbb0b77c62d3dba8ee9de3f97df
bxr <florian.becker@agavos.com>
2012-07-17 16:24:00 +0200

    some new UI elements, click action on tray icon brings window to front

--- OwnDroidApplet.py
+++ OwnDroidApplet.py
@@ -7,6 +7,9 @@ import gnomeapplet
 
 pygtk.require('2.0')
 
+
+XML_PATH = "/home/florian/work/code/OwnDroidClient/gtk_ui.glade"
+
 def create_menu(applet):
     xml="""<popup name="button3"> <menuitem name="ItemPreferences" 
 verb="Preferences" 
@@ -24,28 +27,64 @@ pixname="gtk-about"/>
     verbs = [('About', show_about), ('Preferences', show_preferences)]
     applet.setup_menu(xml, verbs, None)
     
-
+    
 def show_about(*arguments):
-    print(arguments)
+    builder = gtk.Builder()
+    builder.add_from_file(XML_PATH)
+    builder.connect_signals(handlers)
+    window = builder.get_object("ownAboutWindow")
+    response = window.run()
+    window.destroy()
+
 
 def show_preferences(*arguments):
-    print(arguments)
+    builder = gtk.Builder()
+    builder.add_from_file(XML_PATH)
+    builder.connect_signals(handlers)
+    window = builder.get_object("ownSettings")
+    response = window.run()
+    window.destroy()
+    
+def toggle_window(*arguments):
+    global main_window
+    
+    
+    if main_window != 0 :
+        main_window.destroy()
+        main_window=0
+    else:
+        createUI(XML_PATH)
+        
+        
+    
+handlers={"show_about":show_about, "show_preferences":show_preferences }
+main_window=0;
+
+
+def createUI(file_name):
+    builder = gtk.Builder()
+    builder.add_from_file(file_name)
+    builder.connect_signals(handlers)
+    window = builder.get_object("owndroidMain")
+    global main_window
+    main_window = window
+    window.show_all()
+    
 
 def applet_factory(applet, iid):
     create_menu(applet)
    
-    image = gtk.Image();
-    image.set_from_file("/home/florian/work/code/OwnDroidClient/robot.png")
+    image = gtk.Image()
+    image.set_from_file("/home/florian/work/code/OwnDroidClient/android_small.png")
     
-    button = gtk.Button();
-    button.set_image(image);
-    label = gtk.Label("Own-Me \/")
-    label.set
-    #applet.add(gtk.Window("Test"))
-    applet.add(button)
-    applet.add(label)
+    eventBox = gtk.EventBox()
+    eventBox.add(image)
+   
+    #eventBox.set_events(gtk.gdk.BUTTON_PRESS_MASK)
+    eventBox.connect("button_press_event", toggle_window)
+    applet.add(eventBox)
+    
     applet.show_all()
-    
     print('Factory started')
     return True
             
@@ -53,13 +92,16 @@ def applet_factory(applet, iid):
 if __name__ == '__main__':   # testing for execution
     print('Starting factory')
     if len(sys.argv) > 1 and sys.argv[1] == '-d': # debugging
-        mainWindow = gtk.Window()
-        mainWindow.set_title('OwnDroid Debug-Mode')
-        mainWindow.connect('destroy', gtk.main_quit)
-        applet = gnomeapplet.Applet()
-        applet_factory(applet, None)
-        applet.reparent(mainWindow)
-        mainWindow.show_all()
+        #mainWindow = gtk.Window()
+        #mainWindow.set_title('OwnDroid Debug-Mode')
+        #mainWindow.connect('destroy', gtk.main_quit)
+        #applet = gnomeapplet.Applet()
+        #applet_factory(applet, None)
+        #applet.reparent(mainWindow)
+        #mainWindow.show_all()
+        
+        createUI(XML_PATH)
+        
         gtk.main()
         sys.exit()
     else:
--- test.py
+++
@@ -1,8 +0,0 @@
-'''
-Created on 13 Jul 2012
-
-@author: florian
-'''
-
-if __name__ == '__main__':
-    pass
